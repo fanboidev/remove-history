e032b1dbc562415ca3d87b7a12fc598b96a55210
shintaroDesu <snkraishin87@gmail.com>
2018-10-23 17:52:31 +0200

    convert space to tab

--- src/entities/Human.py
+++ src/entities/Human.py
@@ -1,8 +1,11 @@
+#/usr/bin/env python3
+
 class Human:
-    def __init__(self, name, x, y):
-        self.x=x
-        self.y=y
-        self.name=name
-    def move(self,nouveau_y,nouveau_x):
-        self.y=self.y+nouveau_y
-        self.x=self.x+nouveau_x
+	def __init__(self, name, x, y):
+		self.x=x
+		self.y=y
+		self.name=name
+
+	def move(self,nouveau_y,nouveau_x):
+		self.y=self.y+nouveau_y
+		self.x=self.x+nouveau_x
