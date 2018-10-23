10672e2f54e5014669f461b399e9abb0ecfc1e9c
fanboidev <marc-aurele.ferraz@gmx.fr>
2018-10-23 17:45:44 +0200

    mec g fini le jeu il est ouf

---
+++ src/entities/Human.py
@@ -0,0 +1,8 @@
+class Human:
+    def __init__(self, name, x, y):
+        self.x=x
+        self.y=y
+        self.name=name
+    def move(self,nouveau_y,nouveau_x):
+        self.y=self.y+nouveau_y
+        self.x=self.x+nouveau_x
