62f7d913d5b2d48f9443b5529d704f7c18978a3b
fanboidev <marc-aurele.ferraz@gmx.fr>
2018-10-28 16:39:33 +0100

    mabit

--- src/entities/Human.py
+++ src/entities/Human.py
@@ -6,12 +6,6 @@ class Human:
 		self.y=y
 		self.name=name
 
-	def move(self,nouveau_y,nouveau_x):
-		self.y=self.y+nouveau_y
-		self.x=self.x+nouveau_x
-	def draw():
-		print("drawing...")
-
 	def typing():
 		print("hello")
 	
